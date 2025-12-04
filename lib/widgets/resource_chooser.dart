9386e3078057d627d4baebc4edd598a469575c58
ngaretou <corey.garrett@sim.org>
2025-12-04 23:13:36 +0000

    resource language and resource chooser

---
+++ lib/widgets/resource_chooser.dart
@@ -0,0 +1,122 @@
+import 'package:fluent_ui/fluent_ui.dart';
+import 'package:skeletonizer/skeletonizer.dart';
+import '../logic/aquifer_api.dart';
+import '../main.dart';
+import '../providers/aquifer_classes.dart';
+
+class ResourceChooser extends StatefulWidget {
+  final void Function(String) onChanged;
+  final List<String> resourceCodes;
+  final int langId;
+  const ResourceChooser({
+    super.key,
+    required this.onChanged,
+    required this.resourceCodes,
+    required this.langId,
+  });
+
+  @override
+  State<ResourceChooser> createState() => _ResourceChooserState();
+}
+
+class _ResourceChooserState extends State<ResourceChooser> {
+  @override
+  void initState() {
+    super.initState();
+    widget.resourceCodes;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final itemsController = FlyoutController();
+    List<ResourceCollectionInfo> resourceCollections = AquiferService()
+        .getResourcesForLanguage(widget.langId);
+
+    return FlyoutTarget(
+      controller: itemsController,
+      child: IconButton(
+        icon: const Icon(FluentIcons.settings),
+        onPressed: () {
+          itemsController.showFlyout(
+            autoModeConfiguration: FlyoutAutoConfiguration(
+              preferredMode: FlyoutPlacementMode.topCenter,
+            ),
+            barrierDismissible: true,
+            dismissOnPointerMoveAway: false,
+            dismissWithEsc: true,
+            // navigatorKey: rootNavigatorKey.currentState,
+            builder: (context) {
+              return StatefulBuilder(
+                builder: (context, setState) {
+                  return FlyoutContent(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: resourceCollections.map((collection) {
+                        return Padding(
+                          padding: const EdgeInsets.all(6.0),
+                          child: ToggleSwitch(
+                            checked: widget.resourceCodes.contains(
+                              collection.code,
+                            ),
+                            onChanged: (v) {
+                              // setState(() {
+                              //   if (resourceCodes.contains(collection.code)) {
+                              //     resourceCodes.remove(collection.code);
+                              //   } else {
+                              //     resourceCodes.add(collection.code);
+                              //   }
+                              // });
+                              setState(() {
+                                widget.onChanged(collection.code);
+                              });
+                            },
+                            content: Flexible(
+                              child: Text(
+                                collection.availableLanguages
+                                    .where((l) => l.id == widget.langId)
+                                    .first
+                                    .displayName,
+                              ),
+                            ),
+                          ),
+                        );
+                      }).toList(),
+                    ),
+                  );
+                },
+              );
+              // return StatefulBuilder(
+              //   builder: (context, setStateChild) {
+              //     return MenuFlyout(
+              //       items: resourceCollections.map((collection) {
+              //         return ToggleMenuFlyoutItem(
+              //           text: Text(
+              //             collection.availableLanguages
+              //                 .where((l) => l.id == widget.langId)
+              //                 .first
+              //                 .displayName,
+              //           ),
+              //           value: resourceCodes.contains(collection.code),
+              //           onChanged: (v) {
+              //             setState(() {
+              //               if (v) {
+              //                 resourceCodes.add(collection.code);
+              //               } else {
+              //                 resourceCodes.remove(collection.code);
+              //               }
+              //             });
+              //             widget.onChanged(collection.code);
+              //           },
+              //         );
+              //       }).toList(),
+              //     );
+              //   },
+              // );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
